b19860ca3cfa587e094a8798b3363ef7489b573f
Aurélien <rofldu08@gmail.com>
2023-07-25 19:19:16 +0200

    Vue de la page "à propos"

---
+++ vuePropos.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo $titre ?>></title>
+    <link rel="icon" href="assets/icon/points-blancs-cercle-fond-noir-toile-fond-demi-teinte-spirale-abstraite-moderne_541122-271.avif">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<?php include ("WebsitePart/header.php") ?>
+<div class="container">
+
+</div>
+<?php include ("WebsitePart/footer.php") ?>
+</body>
+</html>
