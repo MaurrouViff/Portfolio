62863d7dc1dd0d49c26aef24bfa0f10ec81d4ab4
Aurélien <rofldu08@gmail.com>
2023-07-25 19:17:35 +0200

    fichier qui gère l'affichage de la page des logiciels

--- controlleur/logiciel.php
+++ controlleur/logiciel.php
@@ -2,6 +2,7 @@
 if ($_SERVER["SCRIPT_FILENAME"] == __FILE__) {
     $racine = "..";
 }
+// Gère l'affichage de la page logiciel
 $titre = "Accueil - Portfolio";
 include "$racine/vueLogiciel.php";
 
