861b8179a11b8aa93833c0ad1afefd769902d4af
Aurélien <rofldu08@gmail.com>
2023-08-02 17:20:05 +0200

    Updated website

--- vueLangage.php
+++ vueLangage.php
@@ -15,9 +15,27 @@
     <div class="gradient_policy"><h1>Voici les langages que j'apprends :</h1></div>
     <hr class="hr_gradient">
     <h3 class="white">J'apprends actuellement le langage python</h3>
-    <img class="image" src="assets/images/" alt="Image du langage python">
-    <h3 class="white">J'utilise comme IDE Python : <a class="link-to-page" href="">PyCharm</a></h3>
+    <img class="image" src="assets/images/Python-image.png" alt="Image du langage python">
+    <h3 class="white">J'utilise comme IDE Python : <a class="link-to-page" href="https://www.jetbrains.com/fr-fr/pycharm/">PyCharm</a></h3>
     <img class="image" src="assets/images/PyCharm_Icon.svg.png" alt="Image du logiciel PyCharm">
+    <hr class="hr_gradient">
+    <h3 class="white">J'apprends plein de langage web comme le PHP & JavaScript</h3>
+    <img class="image" src="assets/images/PHP-image.png" alt="Image de PHP">
+    <img class="image" src="assets/images/JS-image.png" alt="Image de JS">
+    <h3 class="white">J'utilise comme IDE pour les langages web : <a class="link-to-page" href="https://www.jetbrains.com/fr-fr/phpstorm/">PhpStorm</a></h3>
+    <img class="image" src="assets/images/PhpStorm_Icon.svg.png" alt="Image de PHPStorm">
+    <hr class="hr_gradient">
+    <h3 class="white">J'apprends le Java</h3>
+    <img class="image" src="assets/images/Java-image.png" alt="Image de Java">
+    <h3 class="white">J'utilise comme IDE Java : IntelliJ IDEA</h3>
+    <img class="image" src="assets/images/IntelliJ_IDEA_Icon.svg.png" alt="Image d'IntelliJ IDEA">
+    <hr class="hr_gradient">
+    <div class="gradient_policy"><h1>Langages que je compte apprendre :</h1></div>
+    <h3 class="white">Je compte apprendre le C# et le C++</h3>
+    <img class="image" src="assets/images/CSharp-image.png" alt="Image de CSharp">
+    <img class="image" src="assets/images/CPP-image.png" alt="Image de C++">
+
+
 </div>
 <!-- Flèche de retour en haut de page -->
 <div class="arrow-container">
