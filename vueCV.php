ae56b4396e90ce129033a373b979c24d36d2e98f
Aurélien <rofldu08@gmail.com>
2023-08-07 13:36:12 +0200

    Updated website

--- vueCV.php
+++ vueCV.php
@@ -16,6 +16,12 @@
     <div class="gradient_policy"><h3>Mes diplômes :</h3></div>
     <h3 class="cyan">Brevet des collèges (2015-2019)</h3>
     <h3 class="cyan">BAC STI2D spécialité S.I.N (2019-2022)</h3>
+    <h3 class="cyan">BTS SIO spécialité SLAM (2022-2024)</h3>
+    <hr class="hr_gradient">
+    <div class="gradient_policy"><h3>Mes expériences professionnelles : </h3></div>
+    <h3 class="cyan">Stage 3ème (2019) au service informatique de ma ville</h3>
+    <h3 class="cyan">Stage BTS SIO 1ère année (2022)</h3>
+    <h3 class="cyan">Stage BTS SIO 2ème année (2023)</h3>
 </div>
 <?php include ("WebsitePart/footer.php") ?>
 </body>
