7cf1fae6c1ce892d7fec01ba76da0462b5b96847
Aurélien <rofldu08@gmail.com>
2023-08-30 18:05:16 +0200

    index du portfolio

--- index.php
+++ index.php
@@ -1,16 +1,63 @@
-<?php
+<!doctype html>
+<html lang="fr" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Accueil - Portfolio</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="assets/icon/icon-portfolio.png">
+</head>
+<body>
 
-include "getRacine.php";
-include "$racine/controlleur/Controller.php";
 
+<header class="header">
+<h2 class="gradient_policy">Bienvenue sur mon portfolio !</h2>
 
-$controller = new Controller();
+    <nav class="navbar">
+        <ul class="nav-menu">
+            <li class="nav-item">
+                <a href="propos.php" class="blue_btn">À propos</a>
+            </li>
+            <li class="nav-item">
+                <a href="langage.php" class="blue_btn">Langages</a>
+            </li>
+            <li class="nav-item">
+                <a href="conception.php" class="blue_btn">Conception</a>
+            </li>
+            <li class="nav-item">
+                <a href="cv.php" class="blue_btn">CV</a>
+            </li>
+        </ul>
+    </nav>
+<hr class="hr_gradient">
+</header>
+<div class="container">
+    <div class="gradient_policy">
+        <h1>Voici quelques</h1>
+        <h1>Informations sur moi : </h1>
+    </div>
+<section class="main-section">
+    <div class="features">
+        <div class="feature">
+            <div class="feature-title"><img alt="image d'étudiant" class="image-icon" src="assets/icon-images/student-icon.png">Étudiant</div>
+            <div class="feature-text">Je suis actuellement étudiant en BTS Sio spécialité SLAM.</div>
+        </div>
+        <div class="feature">
+            <div class="feature-title"><img alt="image de développeur" class="image-icon" src="assets/icon-images/developer-icon.png">Développeur</div>
+            <div class="feature-text">J'apprends certains langages de programmation. <a href="./?action=langage" class="link-to-page">Cliquez ici</a> pour voir la liste</div>
+        </div>
+    </div>
+</section>
 
-if (isset($_GET["action"])) {
-    $action = $_GET["action"];
-} else {
-    $action = "defaut";
-}
+</div>
+<!-- Flèche de retour en haut de la page -->
+<div class="arrow-container">
+    <i class="arrow">↑</i>
+</div>
 
-$fichier = $controller->execute($action);
-include "$racine/controlleur/$fichier";
+<?php include ("WebsitePart/footer.php") ?>
+</body>
+<script type="text/javascript" src="JavaScript/scrollToTop.js"></script>
+</html>
